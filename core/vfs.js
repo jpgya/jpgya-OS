04bed66b7ba9b5413f2986fda208573e6fc24e42
jpgya <vigyanfv@gmail.com>
2025-07-30 12:16:48 +0900

    Update vfs.js

--- core/vfs.js
+++ core/vfs.js
@@ -1,4 +1,4 @@
-export let vfs = JSON.parse(localStorage.getItem("vfs") || '{"files":{}}');
+export let vfs = JSON.parse(localStorage.getItem("vfs") || '{"files":{"readme.txt":"Ultimate Web OSへようこそ！"}}');
 export function saveVFS() {
   localStorage.setItem("vfs", JSON.stringify(vfs));
 }
